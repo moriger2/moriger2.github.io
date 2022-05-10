961fc3479a9cff2f209e4497170cd5ea1e670f72
moriger2 <98115426+moriger2@users.noreply.github.com>
2022-05-10 15:26:12 -0500

    Update and rename Wells.json to Wells.js

--- Wells.js
+++ Wells.js
@@ -1,5 +1,7 @@
 var wells = {
   "type" : "FeatureCollection",
+  "name" : "UBB_wells",
+  "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
   "features" : [
     {
       "type" : "Feature",
